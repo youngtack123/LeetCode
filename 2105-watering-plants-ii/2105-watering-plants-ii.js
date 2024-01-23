d127377dda2fedb2e7eb877a1f73bae20c75ca76
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-01-23 11:25:09 +0900

    Time: 1033 ms (7.69%), Space: 75.8 MB (7.69%) - LeetHub

---
+++ 2105-watering-plants-ii/2105-watering-plants-ii.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[]} plants
+ * @param {number} capacityA
+ * @param {number} capacityB
+ * @return {number}
+ */
+var minimumRefill = function(plants, capacityA, capacityB) {
+    let refillCount = 0;
+    let curCapA = capacityA;
+    let curCapB = capacityB;
+    
+    for(let i = 0, j = plants.length - 1 ; i< plants.length && j > 0 ; i++, j--) {
+        
+        // 동시에 같은 위치에 있을 경우
+        if(i === j) {
+            const oneWithMoreWater = Math.max(curCapA, curCapB) === curCapA ? 'Alice' : 'Bob';
+            
+            if(oneWithMoreWater === 'Bob' && plants[j] > curCapB) {
+                refillCount++;
+            }
+            else if(oneWithMoreWater === 'Alice' && plants[i] > curCapA) {
+                refillCount++;
+            }
+            break;
+        }
+        
+        // 충분한 물이 있을 경우 - Alice
+        if(plants[i] <= curCapA) {
+            curCapA = curCapA - plants[i]
+        } else {
+            curCapA = capacityA;
+            curCapA = curCapA - plants[i]
+            refillCount++;
+        }
+        
+        // 충분한 물이 있을 경우 - Bob
+        if(plants[j] <= curCapB) {
+            curCapB = curCapB - plants[j]
+        } else {
+            curCapB = capacityB;
+            curCapB = curCapB - plants[j]
+            refillCount++;
+        }
+        
+        if(j - i === 1) {
+            break
+        }
+        
+        console.log(i, j, curCapA, curCapB)
+    }
+    return refillCount
+};
