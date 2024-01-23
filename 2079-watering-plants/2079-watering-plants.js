6b52a7444acecb6e9554f5f7854b2d53f722bbe2
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-01-23 09:58:35 +0900

    Time: 39 ms (100.00%), Space: 49.1 MB (5.26%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.js
+++ 2079-watering-plants/2079-watering-plants.js
@@ -10,31 +10,25 @@ var wateringPlants = function(plants, capacity) {
     let total = 0;
     
     for(let i = 0 ; i < plants.length ; i++) {
-//         if(i === 0) {
-//             currentCap = capacity - plants[i]
-//             pointer = i;
-//             total++;
-//         } else {
-//             // 충분한 물이 있을 경우
-            
-            if(plants[i] <= currentCap) {
-                currentCap = currentCap - plants[i]
-                pointer = i
-                total += 1
-            }
-            // 충분한 물이 없을 경우
-            else {
-                const refillDistance = pointer + 1
-                pointer = -1
-                total += refillDistance
-                
-                // 물 채운 후 다시 이동 로직
-                pointer = i
-                currentCap = capacity
-                currentCap = currentCap - plants[i]
-                total += Math.abs(-1 - i)
-            }
+        
+        // 충분한 물이 있을 경우
+        if(plants[i] <= currentCap) {
+            currentCap = currentCap - plants[i]
+            pointer = i
+            total += 1
         }
-    // }
+        // 충분한 물이 없을 경우
+        else {
+            const refillDistance = pointer + 1
+            pointer = -1
+            currentCap = capacity
+            total += refillDistance
+
+            // 물 채운 후 다시 이동 로직
+            pointer = i
+            currentCap = currentCap - plants[i]
+            total += Math.abs(-1 - i)
+        }
+    }
     return total
 };
