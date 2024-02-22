494a662cf165acde1e5216dd4d21bd280b19e63e
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-02-22 14:32:40 +0900

    Time: 126 ms (28.76%), Space: 57 MB (6.19%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.js
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.js
@@ -6,9 +6,9 @@
 var maximizeSum = function(nums, k) {
         
     let answer = 0
+    let sorted = nums.sort((a, b) => b - a)
     
     while(k > 0) {
-        let sorted = nums.sort((a, b) => b - a)
         let max = nums[0]
         answer += max
         
