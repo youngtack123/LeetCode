e73ed81302022c2aeb5c738da3638b0dc4bbfd73
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-02-23 10:39:05 +0900

    Time: 42 ms (95.97%), Space: 48.7 MB (51.88%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.js
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.js
@@ -2,22 +2,36 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkRecord = function(s) {
-    const arr = [...s]
+// var checkRecord = function(s) {
+//     const arr = [...s]
     
-    let aCount = 0
-    let lCount = 0
+//     let aCount = 0
+//     let lCount = 0
 
     
-    for(let i = 0 ; i < arr.length ; i++) {
-        if(arr[i] === 'A') {
-            aCount++;
-        }
+//     for(let i = 0 ; i < arr.length ; i++) {
+//         if(arr[i] === 'A') {
+//             aCount++;
+//         }
         
-        if(arr[i] === 'L' && arr[i + 1] === 'L' && arr[i + 2] === 'L') {
-            lCount++
-        }
+//         if(arr[i] === 'L' && arr[i + 1] === 'L' && arr[i + 2] === 'L') {
+//             lCount++
+//         }
+//     }
+    
+//     return aCount < 2 && lCount === 0
+// };
+
+var checkRecord = function (s) {
+    let absent = s.split("").filter((val) => val == "A");
+    
+    if(absent.length >= 2) {
+        return false  
     }
     
-    return aCount < 2 && lCount === 0
+    if(s.includes("LLL")) {
+        return false   
+    }
+    
+    return true
 };
