8f155eff14fd3fa8a6de0bac20bec70fad5eade2
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-02-23 17:28:07 +0900

    Time: 71 ms (24.42%), Space: 51.5 MB (45.52%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function(mountain) {
+    
+    const answer = []
+    
+    for(let i = 1 ; i < mountain.length - 1 ; i++) {
+        if(mountain[i - 1] < mountain[i] && mountain[i + 1] < mountain[i]) {
+            answer.push(i)
+        }
+    }
+    return answer
+};
