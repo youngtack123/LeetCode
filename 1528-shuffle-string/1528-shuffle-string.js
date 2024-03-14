e459920a92a415918ba260743a5beedb7d21a655
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-03-14 22:21:49 +0900

    Time: 60 ms (49.41%), Space: 50.4 MB (69.94%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+    let arr = new Array(indices);
+    s = s.split("");
+    for (let i = 0; i < s.length; i++) {
+        arr[indices[i]] = s[i];
+    }
+    return arr.join("");
+};
