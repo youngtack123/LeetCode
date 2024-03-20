ea2f1b8b9a8dd1ab0ce478ffe1ae8ab262220bcb
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-03-20 18:08:31 +0900

    Time: 59 ms (58.82%), Space: 51.1 MB (81.86%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+
+var luckyNumbers  = function(matrix) {
+    
+  for(let i = 0; i < matrix.length; i++){
+      let min = Math.min(...matrix[i]),
+      index = matrix[i].indexOf(min);
+
+      if(matrix.every(arr => arr[index] <= min)){
+          return [min];
+      }
+  }    
+    
+  return [];
+};
