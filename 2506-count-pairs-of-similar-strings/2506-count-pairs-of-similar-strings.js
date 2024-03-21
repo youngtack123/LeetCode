92c69eb10b1245481c4ada54cbb3a444667b5a6d
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-03-21 17:13:30 +0900

    Time: 81 ms (76.61%), Space: 56.1 MB (45.41%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.js
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.js
@@ -2,37 +2,19 @@
  * @param {string[]} words
  * @return {number}
  */
-// var similarPairs = function(words) {
-    
-//     let answer = 0
-    
-//     words.sort((a, b) => a.length - b.length);
-    
-//     for(let i = 0 ; i < words.length - 1 ; i++) {
-        
-//         answer++
-        
-//         for(let j = 0 ; j < words[i].length ; j++) {
-//             if(!words[i+1].includes(words[i].charAt(j))) {
-//                 answer--
-//                 break
-//             }
-//         }   
-
-//     }
-    
-//     return answer
-// };
 
 var similarPairs = function ( words ) {
-  words = words.map( w => [...new Set( w )].sort().join( "" ) )
-  let count = 0
-  for ( let i = 0; i < words.length; i++ ) {
-    for ( let j = i + 1; j < words.length; j++ ) {
-      if ( words[i] === words[j] ) {
-        count++
-      }
+    // Set을 활용해서 중복되는 문자열을 제거하고, 정렬후에, 배열의 형태에서 문자열 형태로 합친다
+    words = words.map(w => [...new Set(w)].sort().join(""))    
+
+    let count = 0
+    
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (words[i] === words[j]) {
+                count++
+            }
+        }
     }
-  }
-  return count
+    return count
 }
