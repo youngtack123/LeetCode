65724ac35943a2c1d659679512057d02c3310718
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-03-24 21:10:33 +0900

    Time: 63 ms (78.49%), Space: 52.5 MB (81.59%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    
+    let map = new Map()
+    
+    for(let i = 0 ; i < nums.length ; i++) {
+        if(map.has(nums[i])) {
+            map.set(nums[i], map.get(nums[i]) + 1)
+        } else {
+            map.set(nums[i], 1)
+        }
+    }
+    
+    const sorted = [...map.entries()].sort((a , b) => b[1] - a[1])
+    
+    let answer = []
+    
+    for(let i = 0 ; i < k ; i++) {
+        answer.push(sorted[i][0])
+    }
+    
+    
+    
+    return answer
+};
