62943ecf24a4586f486dc5a4582082b091397804
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-03-24 16:23:37 +0900

    Time: 99 ms (23.86%), Space: 53.5 MB (58.25%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    
+    let map = new Map()
+    
+    for(let i = 0 ; i < s.length ; i++) {
+        if(map.has(s[i])) {
+            map.set(s[i], map.get(s[i]) + 1)
+        } else {
+            map.set(s[i], 1)
+        }
+    }
+    
+    for(let [key, value] of map) {
+        if(value === 1) {
+            return s.indexOf(key)
+        }
+    }
+    return -1
+    
+    
+};
