56aa7fff00a386430af7915172c65ecb829de6a5
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-03-26 17:48:05 +0900

    Time: 111 ms (92.86%), Space: 68.7 MB (64.29%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.js
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.js
@@ -3,48 +3,7 @@
 //  * @param {string[]} senders
 //  * @return {string}
 //  */
-// var largestWordCount = function(messages, senders) {
-    
-//     let map = new Map()
-    
-//     let counts = []
-    
-//     for(let i = 0 ; i < messages.length ; i++) {
-//         const tmp = messages[i].split(' ').length
-//         counts.push(tmp)
-//     }
-    
-//     for(let i = 0 ; i < senders.length ; i++) {
-//         map.set(senders[i], (map.get(senders[i]) || 0) + counts[i])
-//     }
-    
-//     const sorted = [...map.entries()].sort((a , b) => b[1] - a[1])
-    
-//     let maxSecondIndex = 0
-    
-//     for (const entry of sorted) {
-//         maxSecondIndex = Math.max(maxSecondIndex, entry[1]);
-//     }
-    
-//     const answer = []
-    
-//     for(const entry of sorted) {
-//         if(entry[1] === maxSecondIndex) {
-//             answer.push(entry)
-//         }
-//     }
-    
-//     if(answer.length === 1) {
-//         return answer[0][0]
-//     } else {
-//         const sortedAnswer = answer.sort((a , b) => b[0].localeCompare(a[0]))
-        
-//         return sortedAnswer[0][0]
-//     }
-    
-    
-    
-// };
+
 var largestWordCount = function(messages, senders) {
     let wordCounts = new Map();
     
