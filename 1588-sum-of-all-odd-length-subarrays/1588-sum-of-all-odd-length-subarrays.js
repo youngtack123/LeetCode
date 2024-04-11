f4ab00d0d9329f7141fff5b70a6602e42b282cca
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-11 12:18:11 +0900

    Time: 63 ms (28.12%), Space: 49.4 MB (28.68%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+var sumOddLengthSubarrays = function (arr) {
+    let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j += 2) {
+            for (let k = i; k <= j; k++) {
+                result += arr[k];
+            }
+        }
+    }
+
+    return result;
+};
