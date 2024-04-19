722d086ff36f830315f134225410b720341c5e45
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-19 17:30:31 +0900

    Time: 134 ms (38.71%), Space: 67.1 MB (50.00%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.js
@@ -0,0 +1,19 @@
+var maximumUniqueSubarray = function(nums) {
+    let maxScore = 0;
+    let currentScore = 0;
+    let seen = new Set();
+    let start = 0;
+
+    for (let end = 0; end < nums.length; end++) {
+        while (seen.has(nums[end])) {
+            seen.delete(nums[start]);
+            currentScore -= nums[start];
+            start++;
+        }
+        seen.add(nums[end]);
+        currentScore += nums[end];
+        maxScore = Math.max(maxScore, currentScore);
+    }
+
+    return maxScore;
+};
