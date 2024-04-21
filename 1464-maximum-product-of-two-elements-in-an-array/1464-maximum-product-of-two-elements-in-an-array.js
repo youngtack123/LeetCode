f917e6a1c5d5b0bb6231542d605f0e73b122956a
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-21 14:35:42 +0900

    Time: 55 ms (60.66%), Space: 50.2 MB (44.67%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    const sorted = nums.sort((a, b) => b - a)
+    
+    return (sorted[0] - 1) * (sorted[1] - 1)
+};
