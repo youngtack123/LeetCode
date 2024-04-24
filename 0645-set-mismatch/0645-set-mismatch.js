15ec6fc7da664c03bd403f169259b6aa37ad0aaf
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-24 13:56:57 +0900

    Time: 60 ms (89.14%), Space: 59.1 MB (10.37%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -2,30 +2,6 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-// var findErrorNums = function(nums) {
-    
-//     let target = []
-    
-//     const duplicate = nums.filter((num, idx) => nums.indexOf(num) !== idx)
-    
-//     for(let i = 1 ; i <= nums.length ; i++) {
-//         target.push(i)
-//     }
-    
-//     let missing
-    
-//     for(let i = 0 ; i < target.length ; i++) {
-//         if(!nums.includes(target[i])) {
-//             missing = target[i]
-//             break;
-//         }
-//     }
-    
-    
-    
-//     return [duplicate, missing]
-// };
-
 var findErrorNums = function(nums) {
     // Initialize variables to store duplicate and missing numbers
     let dup = -1, missing = -1;
