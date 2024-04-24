d8b7504129cc9e719d694cf91a41709070c8cf6a
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-24 15:43:29 +0900

    Time: 62 ms (39.84%), Space: 52.8 MB (26.42%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+    let totalTime = duration
+    
+    for(let i = 0 ; i + 1 <timeSeries.length ; i++){
+        let diff= timeSeries[i+1] - timeSeries[i]
+        
+        totalTime += diff > duration ? duration : diff 
+    }
+    return totalTime
+    
+};
