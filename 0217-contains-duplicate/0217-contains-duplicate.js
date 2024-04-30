a5658d65c0bcf3f90a13fd0020fd38c1b58662c8
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-30 14:07:14 +0900

    Time: 64 ms (97.31%), Space: 64 MB (15.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -5,5 +5,6 @@
 var containsDuplicate = function(nums) {
     const set = new Set([...nums])
     
-    return nums.length === set.size ?  false :  true
+    return nums.length !== set.size
+    
 };
