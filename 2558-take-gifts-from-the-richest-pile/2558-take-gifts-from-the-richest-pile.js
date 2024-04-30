8c5c636bd8bbcdc3584344a5c4c38628d4ec69b8
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-04-30 13:40:51 +0900

    Time: 82 ms (56.93%), Space: 51.7 MB (55.47%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} gifts
+ * @param {number} k
+ * @return {number}
+ */
+// var pickGifts = function(gifts, k) {
+    
+//     for(let i = 0 ; i < k ; i++) {
+//         const max = Math.max(...gifts)
+//         const sqrt = Math.floor(Math.sqrt(max))
+        
+//         [sqrt, ...gifts.filter(num => num !== 100)];
+//     }
+// };
+
+var pickGifts = function(gifts, k) {
+    for (let i = 0; i < k; i++) {
+        const maxIndex = gifts.indexOf(Math.max.apply(Math, gifts)); // 가장 큰 값의 인덱스 찾기
+        const maxSqrt = Math.floor(Math.sqrt(gifts[maxIndex])); // 가장 큰 값의 제곱근 계산
+        
+        gifts.splice(maxIndex, 1); // 가장 큰 값 제거
+        gifts.unshift(maxSqrt); // 제곱근 값을 배열 맨 앞에 추가
+    }
+    
+    return gifts.reduce((acc, curr) => acc + curr, 0)
+};
