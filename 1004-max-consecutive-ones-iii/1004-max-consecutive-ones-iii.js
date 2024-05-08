c5bba95a9b80e5d0fa0b3c943abe53412ba24acc
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-08 13:48:03 +0900

    Time: 1554 ms (5.04%), Space: 54.7 MB (18.40%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -3,30 +3,58 @@
  * @param {number} k
  * @return {number}
  */
-var longestOnes = function(nums, k) {
+// var longestOnes = function(nums, k) {
     
-    let flip = k
-    let max = 0
+//     let flip = k
+//     let max = 0
     
-    for(let i = 0 ; i < nums.length ; i++) {
-        let count = 0
-        flip = k
+//     for(let i = 0 ; i < nums.length ; i++) {
+//         let count = 0
+//         flip = k
         
-        for(let j = i ; j < nums.length ; j++) {
+//         for(let j = i ; j < nums.length ; j++) {
             
-            if(nums[j] === 0) {
-                if(flip > 0) {
-                    flip--    
-                } else {
-                    break
-                }
-            }
-            count++
+//             if(nums[j] === 0) {
+//                 if(flip > 0) {
+//                     flip--    
+//                 } else {
+//                     break
+//                 }
+//             }
+//             count++
+//         }
+//         if(max < count) {
+//             max = count
+//         }
+//     }
+    
+//     return max
+// };
+var longestOnes = function(nums, k) {
+
+    let start = 0 ;
+    let count = 0 ;
+    let longest  = 0 ;
+    let flip = k;
+
+    for(let windowEnd = 0 ; windowEnd < nums.length; windowEnd++) {
+        const item = nums[windowEnd];
+
+        if(item === 1) {
+            count++;
         }
-        if(max < count) {
-            max = count
+        else if(item === 0 && flip !== 0) {
+            count++;
+            flip--;
+        } else {
+            longest = Math.max(count, longest);
+            windowEnd = start;
+            start++
+            count = 0 ;
+            flip = k
         }
     }
+    longest = Math.max(count, longest)
     
-    return max
+    return longest;
 };
