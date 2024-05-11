fc91d17ad8d3d286304f504a8b19244b9989db69
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-11 15:27:58 +0900

    Time: 63 ms (30.63%), Space: 51.4 MB (29.52%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countGoodSubstrings = function(s) {
+    let good = 0;
+    
+	// Sliding window of fixed size 3, starting from beginning of string
+    for(let left = 0; left < s.length - 2; left++) {
+		// Put characters from window into a Set
+        const window = [s[left], s[left + 1], s[left + 2]];
+        const chars = new Set(window);
+
+		// If each character is distinct, then the Set should be size 3,
+		// which would make it a good substring
+        if(chars.size === 3) good++;
+    }
+    return good;
+};
