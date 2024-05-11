6148af960d92a343f36fcdd8db279916122455bc
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-11 15:29:32 +0900

    Time: 63 ms (30.63%), Space: 49.7 MB (76.94%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
@@ -2,18 +2,34 @@
  * @param {string} s
  * @return {number}
  */
-var countGoodSubstrings = function(s) {
-    let good = 0;
+// var countGoodSubstrings = function(s) {
+//     let good = 0;
     
-	// Sliding window of fixed size 3, starting from beginning of string
-    for(let left = 0; left < s.length - 2; left++) {
-		// Put characters from window into a Set
-        const window = [s[left], s[left + 1], s[left + 2]];
-        const chars = new Set(window);
+// 	// Sliding window of fixed size 3, starting from beginning of string
+//     for(let left = 0; left < s.length - 2; left++) {
+// 		// Put characters from window into a Set
+//         const window = [s[left], s[left + 1], s[left + 2]];
+//         const chars = new Set(window);
 
-		// If each character is distinct, then the Set should be size 3,
-		// which would make it a good substring
-        if(chars.size === 3) good++;
+// 		// If each character is distinct, then the Set should be size 3,
+// 		// which would make it a good substring
+//         if(chars.size === 3) good++;
+//     }
+//     return good;
+// };
+
+var countGoodSubstrings = function(s) {
+    let good = 0
+    
+    for(let i = 0 ; i < s.length - 2 ; i++) {
+        const first = s[i]
+        const second = s[i + 1]
+        const third = s[i + 2]
+        
+        if(first !== second && first !== third && second !== third) {
+            good++
+        }
     }
-    return good;
+    
+    return good
 };
