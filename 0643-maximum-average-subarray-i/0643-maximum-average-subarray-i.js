1339f905e5d96e83fdcd08b0ea71a6482723bb2b
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-12 21:42:37 +0900

    Time: 78 ms (75.95%), Space: 62 MB (81.74%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -6,13 +6,16 @@
 var findMaxAverage = function(nums, k) {
     
     let left = 0
-    let sum = 0
     let maxAverage = -Infinity
+    let sum = 0
     
     for(let right = 0 ; right < nums.length ; right++) {
+        
         sum += nums[right]
+        
         if(right - left + 1 === k) {
-            maxAverage = Math.max(maxAverage, sum / k)
+            let currentAverage = sum / k
+            maxAverage = Math.max(maxAverage, currentAverage)
             sum -= nums[left]
             left++
         }
