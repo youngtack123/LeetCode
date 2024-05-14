51aa024f023b7bae02592002e29a4df6928d979c
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-14 10:06:55 +0900

    Time: 66 ms (49.54%), Space: 54.9 MB (15.17%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,57 +4,53 @@
  * @return {number}
  */
 // var longestOnes = function(nums, k) {
-    
-//     let flip = k
-//     let max = 0
-    
-//     for(let i = 0 ; i < nums.length ; i++) {
-//         let count = 0
-//         flip = k
-        
-//         for(let j = i ; j < nums.length ; j++) {
-            
-//             if(nums[j] === 0) {
-//                 if(flip > 0) {
-//                     flip--    
-//                 } else {
-//                     break
-//                 }
-//             }
-//             count++
+
+//     let start = 0 ;
+//     let count = 0 ;
+//     let longest  = 0 ;
+//     let flip = k;
+
+//     for(let windowEnd = 0 ; windowEnd < nums.length; windowEnd++) {
+//         const item = nums[windowEnd];
+
+//         if(item === 1) {
+//             count++;
 //         }
-//         if(max < count) {
-//             max = count
+//         else if(item === 0 && flip !== 0) {
+//             count++;
+//             flip--;
+//         } else {
+//             longest = Math.max(count, longest);
+//             windowEnd = start;
+//             start++
+//             count = 0 ;
+//             flip = k
 //         }
 //     }
+//     longest = Math.max(count, longest)
     
-//     return max
+//     return longest;
 // };
 var longestOnes = function(nums, k) {
-
-    let start = 0 ;
-    let count = 0 ;
-    let longest  = 0 ;
-    let flip = k;
-
-    for(let windowEnd = 0 ; windowEnd < nums.length; windowEnd++) {
-        const item = nums[windowEnd];
-
-        if(item === 1) {
-            count++;
-        }
-        else if(item === 0 && flip !== 0) {
-            count++;
-            flip--;
-        } else {
-            longest = Math.max(count, longest);
-            windowEnd = start;
-            start++
-            count = 0 ;
-            flip = k
-        }
-    }
-    longest = Math.max(count, longest)
+    let maxLength = 0
+    let zeros = 0
+    let left = 0
     
-    return longest;
-};
+    for(let right = 0 ; right < nums.length ; right++) {
+        if(nums[right] === 0) {
+            zeros++
+        }
+        
+        while(zeros > k) {
+            if(nums[left] === 0) {
+                zeros--
+            }
+            left++
+        }
+        
+        const current = right - left + 1
+        maxLength = Math.max(maxLength, current)
+    }
+    
+    return maxLength
+}
