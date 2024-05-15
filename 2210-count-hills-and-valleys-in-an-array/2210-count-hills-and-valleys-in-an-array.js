e92ee75fad754b269767bd9cf49a9b53b3652e1b
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-15 18:08:56 +0900

    Time: 54 ms (55.93%), Space: 48.8 MB (67.80%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countHillValley = function(nums) {
+    let count = 0;
+    let left = 0;
+
+    for(let i = 1; i < nums.length - 1; i++) {
+        
+        if(nums[i] !== nums[i + 1]) {
+            if(nums[i] > nums[left] && nums[i] > nums[i + 1] || nums[i] < nums[left] && nums[i] < nums[i + 1]) {
+                count++;
+            }
+            left = i;
+        }
+    }
+    return count;
+};
