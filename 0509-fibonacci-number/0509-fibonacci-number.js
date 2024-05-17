b6d7760d14411d426145b3fdefaa21caa93ecfca
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-17 13:30:18 +0900

    Time: 52 ms (70.58%), Space: 48.6 MB (64.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,11 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib = function(n, memo={}) {
     
-    if(n < 2) {
-        return n
-    } else {
-        return fib(n - 1) + fib(n - 2)
-    }
+    if (n in memo) return memo[n]
+    if (n == 0) return 0
+    if (n == 1) return 1
+    
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    
+    return memo[n]
 };
