01b62d91b3a836218af23df44804e7a7dc2e5f8b
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-20 16:53:15 +0900

    Time: 48 ms (80.95%), Space: 48.4 MB (90.48%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.js
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -2,26 +2,16 @@
  * @param {string} palindrome
  * @return {string}
  */
-// var breakPalindrome = function(palindrome) {
-    
-//     if(palindrome.length === 1) return ''
-    
-//     const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    
-    
-    
-// };
-
 var breakPalindrome = function (palindrome) {
-    const len = palindrome.length;
-    if (len === 1) return '';
+    
+    if (palindrome.length === 1) return ''
 
-    for (let i = 0; i < Math.floor(len / 2); i++) {
+    for (let i = 0 ; i < Math.floor(palindrome.length / 2) ; i++) {
         if (palindrome[i] !== 'a') {
             return (
                 palindrome.substring(0, i) + 'a' + palindrome.substring(i + 1)
             );
         }
     }
-    return palindrome.substring(0, len - 1) + 'b';
+    return palindrome.substring(0, palindrome.length - 1) + 'b';
 };
