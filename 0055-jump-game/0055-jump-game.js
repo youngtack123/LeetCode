5b967c5ce226400d374afb3cf9bda50358cc6265
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-05-25 16:36:31 +0900

    Time: 68 ms (43.93%), Space: 53.1 MB (46.47%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    if (nums.length === 1) return true
+
+    let goal = nums.length - 1
+    
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= goal) {
+            goal = i    
+        }
+    }
+
+    return goal === 0
+};
