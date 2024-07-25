bd2178af213478a0fc496b7406050eb066fda63f
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-07-25 15:44:58 +0900

    Time: 58 ms (64.90%), Space: 49.2 MB (72.35%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function(nums, k) {
+    
+    let answer = 0
+    
+    for(let i = 0 ; i < nums.length - 1 ; i++) {
+        for(let j = i + 1 ; j < nums.length ; j++) {
+            if(Math.abs(nums[i] - nums[j]) == k) {
+                answer++
+            }
+        }
+    }
+    
+    return answer
+};
