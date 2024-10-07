f15379e1da78c15913a92881c2c220a10ab6695f
홍영택 <61100629+youngtack123@users.noreply.github.com>
2024-10-07 00:00:38 -0400

    Time: 82 ms (22.69%), Space: 58.5 MB (88.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = prices[0];
-    let maxProfit = 0;
     
-    for (let i = 1; i < prices.length; i++) {
-        const currentPrice = prices[i];
-        minPrice = Math.min(minPrice, currentPrice);
-        maxProfit = Math.max(maxProfit, currentPrice - minPrice);
+    let currentPrice = prices[0]
+    let maxProfit = 0
+    
+    for(let i = 1 ; i < prices.length ; i++) {
+        if(currentPrice > prices[i]) {
+            currentPrice = prices[i]
+        }
+        maxProfit = Math.max(maxProfit, prices[i] - currentPrice)
     }
     
-    return maxProfit;
+    return maxProfit
 };
